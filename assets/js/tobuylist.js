fcbc04064003b44016a4e80c2717b15b13c0235d
Effie Manyos <fmanyos@gmail.com>
2021-05-08 10:33:28 +0000

    improve the bookstobuy app javascript file

--- assets/js/tobuylist.js
+++ assets/js/tobuylist.js
@@ -1,10 +1,10 @@
 // Book Class: Represents a Book
 class Book {
-    constructor(title, author, isbn, price) {
+    constructor(title, author, price, isbn) {
         this.title = title;
         this.author = author;
-        this.isbn = isbn;
         this.price = price;
+        this.isbn = isbn;
     }
 }
 
@@ -22,12 +22,12 @@ class UI {
         const row = document.createElement('tr');
 
         row.innerHTML = `
-            <td>${book.title}</td>
-            <td>${book.author}</td>
-            <td>${book.isbn}</td>
-            <td>${book.price}</td>
-            <td><a href="#" class="btn btn-sm delete"><i class="fa fa-trash trash-styling"></i></a></td>
-        `;
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.price}</td>
+      <td>${book.isbn}</td>
+      <td><a href="#" class="btn btn-sm delete"><i class="fa fa-trash trash-styling"></i></a></td>
+    `;
 
         list.appendChild(row);
     }
@@ -44,60 +44,98 @@ class UI {
         div.appendChild(document.createTextNode(message));
         const container = document.querySelector('.container');
         const form = document.querySelector('#book-form');
-        container.insertBefore(div, form); // Insert div before the form
-        // Vanish in 3 seconds
+        container.insertBefore(div, form); // Insert the div before the form
+
+        // Message will vanish in 3 seconds
         setTimeout(() => document.querySelector('.alert').remove(), 3000);
     }
 
-    static clearFields() { // THIS IS NOT WORKING ANYMORE
+    static clearFields() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
-        document.querySelector('#isbn').value = '';
         document.querySelector('#price').value = '';
+        document.querySelector('#isbn').value = '';
     }
 }
 
 // Store Class: Handles Local Storage
+class Store {
+    static getBooks() {
+        let books;
+        if (localStorage.getItem('books') === null) {
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+
+        return books;
+    }
+
+    static addBook(book) {
+        const books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn) {
+        const books = Store.getBooks();
+
+        books.forEach((book, index) => {
+            if (book.isbn === isbn) {
+                books.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
 
 // Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
 document.querySelector('#book-form').addEventListener('submit', (e) => {
+
     // Prevent actual submit
     e.preventDefault();
 
     // Get form values
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
-    const isbn = document.querySelector('#isbn').value;
     const price = document.querySelector('#price').value;
+    const isbn = document.querySelector('#isbn').value;
 
     // Validate input has a text
-    if (title === '' || author === '' || isbn === '' || price === '') {
+    if (title === '' || author === '' || price === '' || isbn === '') {
         UI.showAlert('Please fill in all fields', 'danger');
     } else {
-        // Instatiate book
-        const book = new Book(title, author, isbn, price);
 
-        // Add book to UI
+        // Instatiate book
+        const book = new Book(title, author, price, isbn);
+
+        // Add Book to UI
         UI.addBookToList(book);
 
         // Add book to store
         Store.addBook(book);
 
-        // Success message
-        UI.showAlert('The book has been added', 'success'); // THIS IS NOT WORKING ANYMORE
+        // Show success message
+        UI.showAlert('Book Added', 'success');
 
         // Clear fields
-        UI.clearFields(); // THIS IS NOT WORKING ANYMORE
+        UI.clearFields();
     }
 });
 
-// Event: Remove a Book
+// Event: Remove a Book (Event Propagation)
 document.querySelector('#book-list').addEventListener('click', (e) => {
+
+    // Remove book from UI
     UI.deleteBook(e.target);
 
-    // Success message
-    UI.showAlert('The book has been removed', 'warning');
+    // Remove book from store
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+
+    // Show success message
+    UI.showAlert('Book Removed', 'success');
 });

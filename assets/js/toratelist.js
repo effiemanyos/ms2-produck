33566a3cad00367597083abe4d7148ead944eb17
Effie Manyos <fmanyos@gmail.com>
2021-05-08 15:35:50 +0000

    align all files to display toratelist name and share same structure as other apps

---
+++ assets/js/toratelist.js
@@ -0,0 +1,62 @@
+// Initial Ratings
+    const ratings = {
+      hooked: 0,
+      probook: 0,
+      mindset: 0,
+      measure: 0,
+      crossing: 0
+    }
+
+    // Total Stars
+    const starsTotal = 5;
+
+    // Run getRatings when DOM loads
+    document.addEventListener('DOMContentLoaded', getRatings);
+
+    // Form Elements
+    const bookSelect = document.getElementById('book-select');
+    const ratingControl = document.getElementById('rating-control');
+
+    // Init book
+    let book;
+
+    // Book select change
+    bookSelect.addEventListener('change', (e) => {
+      book = e.target.value;
+      // Enable rating control
+      ratingControl.disabled = false;
+      ratingControl.value = ratings[book];
+    });
+
+    // Rating control change
+    ratingControl.addEventListener('blur', (e) => {
+      const rating = e.target.value;
+
+      // Make sure it's 5 or under
+      if (rating > 5) {
+        alert('Please rate 1 - 5');
+        return;
+      }
+
+      // Change rating
+      ratings[book] = rating;
+
+      getRatings();
+    });
+
+    // Get ratings
+    function getRatings() {
+      for (let rating in ratings) {
+        // Get percentage
+        const starPercentage = (ratings[rating] / starsTotal) * 100;
+
+        // Round to nearest 10
+        const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
+
+        // Set width of stars-inner to percentage
+        document.querySelector(`.${rating} .stars-inner`).style.width = starPercentageRounded;
+
+        // Add number rating
+        document.querySelector(`.${rating} .number-rating`).innerHTML = ratings[rating];
+      }
+    }

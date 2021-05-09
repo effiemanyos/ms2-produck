f3ad6ae1b51626f77ec0d756966308ea5661c950
Effie Manyos <fmanyos@gmail.com>
2021-05-09 12:14:55 +0000

    improve the functionality of the bookmarks app

--- assets/js/bookmarks.js
+++ assets/js/bookmarks.js
@@ -1,34 +1,43 @@
-// Listen for form submit
+// Listen for form submit when clicking on the button
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
-// Save Bookmark
+// Save each bookmark
 function saveBookmark(e){
-  // Get form values
-  var siteName =document.getElementById('siteName').value;
-  var siteUrl =document.getElementById('siteUrl').value;
 
-  if(!validateForm(siteName, siteUrl)){
+  // Get form values
+  var websiteName =document.getElementById('websiteName').value;
+  var websiteUrl =document.getElementById('websiteUrl').value;
+
+  if(!validateForm(websiteName, websiteUrl)){
     return false;
   }
 
+  // Object that we submit to localStorage
   var bookmark = {
-    name: siteName,
-    url: siteUrl
+    name: websiteName,
+    url: websiteUrl
   }
 
   // Test if bookmarks is null
   if(localStorage.getItem('bookmarks') === null){
-    // Init array
+
+    // Initialize an array
     var bookmarks = [];
-    // Add to array
+
+    // Add the bookmarks to the array
     bookmarks.push(bookmark);
-    // Set to localStorage
+
+    // Set to localStorage (turn JSON array into string before it's saved)
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
   } else {
+
     // Get bookmarks from localStorage
     var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-    // Add bookmark to array
+
+    // Add bookmark that is submitted to the array
     bookmarks.push(bookmark);
+
     // Re-set back to localStorage
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   }
@@ -39,17 +48,20 @@ function saveBookmark(e){
   // Re-fetch bookmarks
   fetchBookmarks();
 
-  // Prevent form from submitting
+  // Prevent form from submitting (otherwise it flashes)
   e.preventDefault();
 }
 
 // Delete bookmark
 function deleteBookmark(url){
+
   // Get bookmarks from localStorage
   var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
   // Loop through the bookmarks
   for(var i =0;i < bookmarks.length;i++){
     if(bookmarks[i].url == url){
+
       // Remove from array
       bookmarks.splice(i, 1);
     }
@@ -63,8 +75,10 @@ function deleteBookmark(url){
 
 // Fetch bookmarks
 function fetchBookmarks(){
+
   // Get bookmarks from localStorage
   var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
   // Get output id
   var bookmarksResults = document.getElementById('bookmarksResults');
 
@@ -74,9 +88,9 @@ function fetchBookmarks(){
     var name = bookmarks[i].name;
     var url = bookmarks[i].url;
 
-    bookmarksResults.innerHTML += '<div class="well">'+
-                                  '<h3>'+name+
-                                  ' <a class="btn btn-default" target="_blank" href="'+addhttp(url)+'">Visit</a> ' +
+    bookmarksResults.innerHTML += '<div class="results-list">'+
+                                  '<h3> '+name+
+                                  ' <a class="btn btn-default" target="_blank" href="'+addhttp(url)+'"><i class="fa fa-external-link-square"></i></a> ' +
                                   ' <a onclick="deleteBookmark(\''+url+'\')" class="btn bm-btn-danger" href="#">Delete</a> ' +
                                   '</h3>'+
                                   '</div>';
@@ -84,8 +98,8 @@ function fetchBookmarks(){
 }
 
 // Validate Form
-function validateForm(siteName, siteUrl){
-  if(!siteName || !siteUrl){
+function validateForm(websiteName, websiteUrl){
+  if(!websiteName || !websiteUrl){
     alert('Please fill in the form');
     return false;
   }
@@ -93,7 +107,7 @@ function validateForm(siteName, siteUrl){
   var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
   var regex = new RegExp(expression);
 
-  if(!siteUrl.match(regex)){
+  if(!websiteUrl.match(regex)){
     alert('Please use a valid URL');
     return false;
   }
